245caf07d32f967858f4f15d9caf0ba07721579c
chocoacocoa <vancholo2006@gmail.com>
2024-12-08 14:48:11 +0800

    use frontend/web/bhsregsys

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -1,24 +1,20 @@
-<?php
-
-/** @var yii\web\View $this */
-
-$this->title = 'My Yii Application';
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BHS Registry System</title>
-    <link rel="stylesheet" href="/registry.system/style.css">
-    <link rel="icon" href="/images/logo.png">
+    <!-- <link rel="stylesheet" href="/registry.system/style.css"> -->
+     <link rel="stylesheet" href="<?= Yii::$app->request->baseUrl ?>/css/style.css">
+    <link rel="icon" href="/registry.system/images/logo.png">
     <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
 
 </head>
 <body>
-    <!-- <header>
+    <header>
         <a href="">
-            <img src="/images/logo.png" alt="BHS REGISTRY SYSTEM LOGO">
+            <!-- <img src="/registry.system/images/logo.png" alt="BHS REGISTRY SYSTEM LOGO"> -->
+            <img src="<?= Yii::$app->request->baseUrl?>/images/images/logo.png" alt="BHS REGISTRY SYSTEM LOGO">
         </a>
         <nav>
             <ul>
@@ -33,9 +29,9 @@ $this->title = 'My Yii Application';
         </nav>
         <div class="registration">
             <a href="" class="sign-up">Sign up</a>
-            <button class="btn-yellow">Log In</button>
+            <button class="btn-yellow" id="">Log In</button>
         </div>
-    </header> -->
+    </header>
     <main>
         <div id="hero">
             <div class="hero-text">
@@ -44,7 +40,7 @@ $this->title = 'My Yii Application';
                     <h2>Registry System</h2>
                 </div>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione doloribus <br> illo, vel repellendus quas laudantium quisquam perspiciatis <br> culpa ex sed ipsum ab illum ipsam optio hic ad est iusto cum.</p>
-                <button class="btn-yellow learn">Learn more</button>
+                <button class="btn-yellow learn" id="goAbout">Learn more</button>
             </div>
             <div class="hero-img">
             </div>
@@ -65,8 +61,9 @@ $this->title = 'My Yii Application';
             <h2 class="people-ttl">Website Developers</h2>
             <div class="card-container">
                 <div class="card card1">
-                    <img src="/images/cholo-card.png" alt="Van Cholo Esguerra">
-                    <div class="links">
+                    <!-- <img src="/registry.system/images/cholo-card.png" alt="Van Cholo Esguerra"> -->
+                    <img src="<?= Yii::$app->request->baseUrl?>/images/images/cholo-card.png" alt="BHS REGISTRY SYSTEM LOGO">
+                    <div class="links5">
                         <a href="https://chocoacocoa.github.io/Van-Cholo---Profile/" target="_blank"><i class="ri-pages-line"></i></a>
                         <a href="https://www.facebook.com/vancholo.esguerra.5/" target="_blank"><i class="ri-facebook-fill"></i></a>
                         <a href="https://github.com/chocoacocoa" target="_blank"><i class="ri-github-fill"></i></a>
@@ -75,22 +72,22 @@ $this->title = 'My Yii Application';
                     </div>
                 </div>
                 <div class="card card2">
-                    <img src="/images/cudia-card.png" alt="Lucky Vincent Cudia">
-                    <div class="links">
+                    <img src="/registry.system/images/cudia-card.png" alt="Lucky Vincent Cudia">
+                    <div class="links3">
                         <a href="https://www.facebook.com/luckyvincent.cudia?mibextid=ZbWKwL" target="_blank"><i class="ri-facebook-fill"></i></a>
                         <a href="https://github.com/Cudia0" target="_blank"><i class="ri-github-fill"></i></a>
                         <a href="https://youtube.com/@luckyvincentcudia9764" target="_blank"><i class="ri-youtube-line"></i></a>
                     </div>
                 </div>
                 <div class="card card3">
-                    <img src="/images/ken-card.png" alt="Julius Peralta">
+                    <img src="/registry.system/images/ken-card.png" alt="Julius Peralta">
                     <div class="links">
                         <a href="#" target="_blank"><i class="ri-facebook-fill"></i></a>
                     </div>
                 </div>
                 <div class="card card4">
-                    <img src="/images/rogon-card.png" alt="Leonel Rogon">
-                    <div class="links">
+                    <img src="/registry.system/images/rogon-card.png" alt="Leonel Rogon">
+                    <div class="links5">
                         <a href="https://rogondotdev.github.io/personal-portfolio/" target="_blank"><i class="ri-pages-line"></i></a>
                         <a href="https://www.facebook.com/rogon.dev" target="_blank"><i class="ri-facebook-fill"></i></a>
                         <a href="https://github.com/rogondotdev" target="_blank"><i class="ri-github-fill"></i></a>
@@ -99,7 +96,7 @@ $this->title = 'My Yii Application';
                     </div>
                 </div>
                 <div class="card card5">
-                    <img src="/images/ken-card.png" alt="Kenneth Apolong">
+                    <img src="/registry.system/images/ken-card.png" alt="Kenneth Apolong">
                     <div class="links">
                         <a href="#" target="_blank"><i class="ri-facebook-fill"></i></a>
                     </div>
@@ -108,36 +105,36 @@ $this->title = 'My Yii Application';
 
             <div class="card-container">
                 <div class="card card1">
-                    <img src="/images/toledo-card.png" alt="Hillary Mhyles Toledo">
-                    <div class="links">
+                    <img src="/registry.system/images/toledo-card.png" alt="Hillary Mhyles Toledo">
+                    <div class="links3">
                         <a href="https://www.facebook.com/share/6s5ZNDhg7dAgBaFA/?mibextid=LQQJ4d" target="_blank"><i class="ri-facebook-fill"></i></a>
                         <a href="https://www.instagram.com/colorshin77?igsh=Y3d0dXJ3NWQ4eHdm&utm_source=qr" target="_blank"><i class="ri-instagram-line"></i></a>
                         <a href="https://l.facebook.com/l.php?u=https%3A%2F%2Fgithub.com%2FButterHoney-mhyles890%3Ffbclid%3DIwZXh0bgNhZW0CMTEAAR0LG2GU1KVdao7r_zO5QtDGX6dCIjl4H8TfhPERIsXVLY60xQByUGvHiAc_aem_5XnEy5oKPOZgzw2LvGxI6Q&h=AT13Sy0dVPHXLFDHLsSwdfaE8InLUZ52SaoK_wz-j6W0-Fd568AfLG44lgdpbA8IwMNje2k4gYakmwebUT9wkm0rp1jwQN-5qNYhbyHm4sNbXW29o5GAXzpXezFXtrGKCWrZd_R-8WNqXA0&s=1" target="_blank"><i class="ri-github-fill"></i></a>
                     </div>
                 </div>
                 <div class="card card2">
-                    <img src="/images/elaiza-card.png" alt="Elaize Rosario">
-                    <div class="links">
+                    <img src="/registry.system/images/elaiza-card.png" alt="Elaize Rosario">
+                    <div class="links2">
                         <a href="https://www.facebook.com/elaizajane.rosario" target="_blank"><i class="ri-facebook-fill"></i></a>
                         <a href="https://github.com/ElaizaJanee" target="_blank"><i class="ri-github-fill"></i></a>
                     </div>
                 </div>
                 <div class="card card3">
-                    <img src="/images/ken-card.png" alt="Kenneth Apolong">
+                    <img src="/registry.system/images/jundy-card.png" alt="Sir Jundy Dimasu-ay">
                     <div class="links">
                         <a href="#" target="_blank"><i class="ri-facebook-fill"></i></a>
                     </div>
                 </div>
                 <div class="card card4">
-                    <img src="/images/eloisa-card.png" alt="Eloisa Rosario">
-                    <div class="links">
+                    <img src="/registry.system/images/eloisa-card.png" alt="Eloisa Rosario">
+                    <div class="links3">
                         <a href="https://www.facebook.com/eloisajean.rosario.54" target="_blank"><i class="ri-facebook-fill"></i></a>
                         <a href="https://github.com/LilJeanie" target="_blank"><i class="ri-github-fill"></i></a>
                         <a href="https://www.instagram.com/eloisajean.14" target="_blank"><i class="ri-instagram-line"></i></a>
                     </div>
                 </div>
                 <div class="card card5">
-                    <img src="/images/erin-card.png" alt="Erin Alonzo">
+                    <img src="/registry.system/images/erin-card.png" alt="Erin Alonzo">
                     <div class="links">
                         <a href="https://www.facebook.com/share/1DXsqNLEfu/?mibextid=qi2Omg" target="_blank"><i class="ri-facebook-fill"></i></a>
                     </div>
@@ -149,7 +146,7 @@ $this->title = 'My Yii Application';
     <footer>
         <div class="footer-container">
             <div class="logo-foot">
-                <img src="/images/logo.png" alt="Balingasa Higb School Registry System">
+                <img src="/registry.system/images/logo.png" alt="Balingasa Higb School Registry System">
             </div>
             <div class="links-foot">
                 <h2>WEBSITE DEVELOPERS | SPECIAL MENTIONS</h2>
@@ -205,67 +202,10 @@ $this->title = 'My Yii Application';
         </div>
         &copy; 2024 All Rights Reserved <br> Balingasa High School 12-ICT OCTAV8
     </footer>
+    <script>
+        document.getElementById("goAbout").addEventListener("click", function() {
+    window.location.href = "#about";
+  });
+    </script>
 </body>
 </html>
-
-
-<!-- <div class="site-index">
-    <div class="p-5 mb-4 bg-transparent rounded-3">
-        <div class="container-fluid py-5 text-center">
-            <h1 class="display-4">
-                <?= $text ?>
-            </h1>
-            <p class="fs-5 fw-light">You have successfully created your Yii-powered application.</p>
-            <p><a class="btn btn-lg btn-success" href="https://www.yiiframework.com">Get started with Yii</a></p>
-        </div>
-    </div>
-
-    <div class="body-content">
-
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
-                    et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                    aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-                    dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/doc/">Yii Documentation
-                        &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
-                    et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                    aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-                    dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/forum/">Yii Forum &raquo;</a>
-                </p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
-                    et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                    aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-                    dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/extensions/">Yii Extensions
-                        &raquo;</a></p>
-            </div>
-        </div>
-
-    </div>
-</div> -->
