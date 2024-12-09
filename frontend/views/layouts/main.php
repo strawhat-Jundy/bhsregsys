560bf00f1e6983f40a751e9d7f14ff3be42a5799
Cudia0 <luckyvincentcudia@gmail.com>
2024-12-09 08:27:17 +0800

    fixedHeaderandRemovedFooterInlayout/main.php

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -27,7 +27,7 @@ AppAsset::register($this);
 
 <header>
         <a href="">
-            <img src="/images/logo.png" alt="BHS REGISTRY SYSTEM LOGO">
+            <img src="/images/logo.png" >
         </a>
         <nav>
             <ul>
@@ -55,13 +55,13 @@ AppAsset::register($this);
         <?= $content ?>
     </div>
 </main>
-
+<!-- 
 <footer class="footer mt-auto py-3 text-muted">
     <div class="container">
         <p class="float-start">&copy; <?= Html::encode(Yii::$app->name) ?> <?= date('Y') ?></p>
         <p class="float-end"><?= Yii::powered() ?></p>
     </div>
-</footer>
+</footer> -->
 
 <?php $this->endBody() ?>
 </body>
