df8773bfb5bb883c4ccf6413cc2539c465adc345
chocoacocoa <vancholo2006@gmail.com>
2024-12-10 10:04:02 +0800

    minimal changes

--- frontend/models/subject/subjectsSearch.php
+++ frontend/models/subject/subjectsSearch.php
@@ -93,74 +93,74 @@ class SubjectsSearch extends BalingasaHighSchoolSubjects
 
 
 
-/*<?php
+// <?php
 
-namespace frontend\models\subject;
+// namespace frontend\models\subject;
 
-use yii\base\Model;
-use yii\data\ActiveDataProvider;
-use frontend\models\BalingasaHighSchoolSubjects;
+// use yii\base\Model;
+// use yii\data\ActiveDataProvider;
+// use frontend\models\BalingasaHighSchoolSubjects;
 
-/**
- * subjectsSearch represents the model behind the search form of `frontend\models\BalingasaHighSchoolSubjects`.
+// /**
+//  * subjectsSearch represents the model behind the search form of `frontend\models\BalingasaHighSchoolSubjects`.
  
-class subjectsSearch extends BalingasaHighSchoolSubjects
-{
-    /**
-     * {@inheritdoc}
+// class subjectsSearch extends BalingasaHighSchoolSubjects
+// {
+//     /**
+//      * {@inheritdoc}
      
-    public function rules()
-    {
-        return [
-            [['subject_id'], 'integer'],
-            [['subject_name', 'schedule_day', 'schedule_time', 'room'], 'safe'],
-        ];
-    }
+//     public function rules()
+//     {
+//         return [
+//             [['subject_id'], 'integer'],
+//             [['subject_name', 'schedule_day', 'schedule_time', 'room'], 'safe'],
+//         ];
+//     }
 
-    /**
-     * {@inheritdoc}
+//     /**
+//      * {@inheritdoc}
      
-    public function scenarios()
-    {
-        // bypass scenarios() implementation in the parent class
-        return Model::scenarios();
-    }
+//     public function scenarios()
+//     {
+//         // bypass scenarios() implementation in the parent class
+//         return Model::scenarios();
+//     }
 
-    /**
-     * Creates data provider instance with search query applied
-     *
-     * @param array $params
-     *
-     * @return ActiveDataProvider
+//     /**
+//      * Creates data provider instance with search query applied
+//      *
+//      * @param array $params
+//      *
+//      * @return ActiveDataProvider
      
-    public function search($params)
-    {
-        $query = BalingasaHighSchoolSubjects::find();
+//     public function search($params)
+//     {
+//         $query = BalingasaHighSchoolSubjects::find();
 
-        // add conditions that should always apply here
+//         // add conditions that should always apply here
 
-        $dataProvider = new ActiveDataProvider([
-            'query' => $query,
-        ]);
+//         $dataProvider = new ActiveDataProvider([
+//             'query' => $query,
+//         ]);
 
-        $this->load($params);
+//         $this->load($params);
 
-        if (!$this->validate()) {
-            // uncomment the following line if you do not want to return any records when validation fails
-            // $query->where('0=1');
-            return $dataProvider;
-        }
+//         if (!$this->validate()) {
+//             // uncomment the following line if you do not want to return any records when validation fails
+//             // $query->where('0=1');
+//             return $dataProvider;
+//         }
 
-        // grid filtering conditions
-        $query->andFilterWhere([
-            'subject_id' => $this->subject_id,
-        ]);
+//         // grid filtering conditions
+//         $query->andFilterWhere([
+//             'subject_id' => $this->subject_id,
+//         ]);
 
-        $query->andFilterWhere(['like', 'subject_name', $this->subject_name])
-            ->andFilterWhere(['like', 'schedule_day', $this->schedule_day])
-            ->andFilterWhere(['like', 'schedule_time', $this->schedule_time])
-            ->andFilterWhere(['like', 'room', $this->room]);
+//         $query->andFilterWhere(['like', 'subject_name', $this->subject_name])
+//             ->andFilterWhere(['like', 'schedule_day', $this->schedule_day])
+//             ->andFilterWhere(['like', 'schedule_time', $this->schedule_time])
+//             ->andFilterWhere(['like', 'room', $this->room]);
 
-        return $dataProvider;
-    }
-}
+//         return $dataProvider;
+//     }
+// }
