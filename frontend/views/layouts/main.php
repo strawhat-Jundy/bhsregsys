feeae2dc8f77d522db5470d5e254feb03360e77c
Cudia0 <luckyvincentcudia@gmail.com>
2024-12-10 23:53:33 +0800

    MigrateResponsiveHTMLandCSSfromLeoToFramework

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -23,95 +23,190 @@ AppAsset::register($this);
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
     <style>
-        header {
-            display: flex;
-            align-items: center;
-            justify-content: space-evenly;
-            background-color: #001840;
-            /* border-bottom: 2px solid #efefefb6; */
-            box-shadow: 0 0 1em rgba(255, 255, 255, 0.693);
-            position: fixed;
-            top: 0;
-            right: 0;
-            left: 0;
-            font-family: 'Manrope', sans-serif;
-            font-weight: 600;
-            z-index: 999;
-        }
+        
+/* HEADER SECTION CSS */
 
-        header img {
-            width: 75px;
-            cursor: pointer;
-        }
+header {
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  background-color: #001840;
+  box-shadow: 0 0 1em rgba(255, 255, 255, 0.693);
+  position: fixed;
+  top: 0;
+  right: 0;
+  left: 0;
+  font-family: "Manrope", sans-serif;
+  font-weight: 600;
+  z-index: 999;
+}
+header img {
+  width: 75px;
+  cursor: pointer;
+}
+nav ul {
+  display: flex;
+}
+nav li {
+  list-style: none;
+  font-size: 1rem;
+}
+nav li > a {
+  padding-inline: 0.7em;
+  padding-block: 1.2em;
+  color: #efefef;
+  text-decoration: none;
+  cursor: pointer;
+  transition: all 0.2s ease;
+}
+nav li > a:hover {
+  background-color: #02358e4b;
+  opacity: 0.8;
+}
+.registration {
+  display: flex;
+  align-items: center;
+  gap: 1em;
+}
+.sign-up {
+  color: #efefef;
+  text-decoration: none;
+  cursor: pointer;
+  transition: all 0.2s ease;
+}
+.sign-up:hover {
+  opacity: 0.6;
+}
+.btn-yellow {
+  color: #1f1f1f;
+  text-decoration: none;
+  background-color: #ffb915;
+  padding: 0.7em 1.2em;
+  border-radius: 5%;
+  font-size: 1rem;
+  font-weight: 700;
+  transition: all 0.2s ease;
+  border: none;
+  cursor: pointer;
+  box-shadow: 0 0 1em rgba(0, 0, 0, 0.6);
+}
+.btn-yellow:hover {
+  opacity: 0.8;
+}
+.sidebar {
+  background-color: #001740dc;
+  position: fixed;
+  top: 0;
+  right: 0;
+  padding: 3em 1.5em 1.5em;
+  height: 100vh;
+  width: 250px;
+  z-index: 999;
+  display: none;
+  flex-direction: column;
+  gap: 5em;
+  box-shadow: 0 0 3em black;
+}
+.sidebar > li {
+  padding: 0.8em;
+}
+.ekis {
+  display: flex;
+  justify-content: flex-end;
+}
+.sidebarnav {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1.5em;
+}
+.ri-close-line {
+  color: #efefef;
+  font-size: 2rem;
+  cursor: pointer;
+  transition: all 0.2s ease;
+}
+.ri-close-line:hover {
+  background-color: #02358e4b;
+  opacity: 0.8;
+}
+.menu-button {
+  color: #efefef;
+  list-style: none;
+  cursor: pointer;
+  transition: all 0.2s ease;
+  font-size: 1.8em;
+  display: none;
+}
+.copy {
+  color: #a8a8a8;
+  font-size: 0.8rem;
+  text-align: center;
+  margin-top: 8em;
+}
+@media (max-width: 945px) {
+  header img {
+    width: 60px;
+  }
+  .menu-button {
+    display: inline;
+  }
+  .hideon-mobile {
+    display: none;
+  }
+}
 
-        nav ul {
-            display: flex;
-        }
-
-        nav li {
-            list-style: none;
-            font-size: 1rem;
-
-        }
-
-        nav li>a {
-            padding-inline: .7em;
-            padding-block: 1.2em;
-            color: #efefef;
-            text-decoration: none;
-            cursor: pointer;
-            transition: all 0.2s ease;
-        }
-
-        nav li>a:hover {
-            background-color: #02358e4b;
-            opacity: 0.8;
-        }
-
-        .registration {
-            display: flex;
-            align-items: center;
-            gap: 1em;
-        }
-
-        .sign-up {
-            color: #efefef;
-            text-decoration: none;
-            cursor: pointer;
-            transition: all 0.2s ease;
-        }
-
-        .sign-up:hover {
-            opacity: 0.6;
-        }
-
-        .btn-yellow {
-            color: #1f1f1f;
-            text-decoration: none;
-            background-color: #ffb915;
-            padding: .7em 1.2em;
-            border-radius: 5%;
-            font-size: 1rem;
-            font-weight: 700;
-            transition: all 0.2s ease;
-            border: none;
-            cursor: pointer;
-            box-shadow: 0 0 1em rgba(0, 0, 0, 0.6);
-        }
-
-        .btn-yellow:hover {
-            opacity: 0.8;
-        }
-        .container{
-            margin: 0 auto !important;
-        }
     </style>
 </head>
 
 <body class="d-flex flex-column h-100">
     <?php $this->beginBody() ?>
 
+
     <header>
+        <a href="">
+            <img src="images/logo.png" alt="BHS REGISTRY SYSTEM LOGO">
+        </a>
+        <nav>
+            <ul class="sidebar">
+                <div class="ekis">
+                    <li onclick="hideSidebar()"><i class="ri-close-line"></i></li>
+                </div>
+                <div class="sidebarnav">
+                    <li onclick="hideSidebar"><a href="">Home</a></li>
+                    <li onclick="hideSidebar"><a href="">Student</a></li>
+                    <li onclick="hideSidebar"><a href="">Schedule</a></li>
+                    <li onclick="hideSidebar"><a href="">Room</a></li>
+                    <li onclick="hideSidebar"><a href="">Teacher</a></li>
+                    <li onclick="hideSidebar"><a href="">Subject</a></li>
+                    <li onclick="hideSidebar"><a href="">Summary</a></li>
+                    <li onclick="hideSidebar"><a href="">Sign up</a></li>
+                    <li onclick="hideSidebar" class="login"><a href="">Log In</a></li>
+                </div>
+                <div class="copy">
+                    &copy; 2024 All Rights Reserved <br> Balingasa High School 12-ICT OCTAV8
+                </div>
+            </ul>
+            <ul class="hideon-mobile">
+                <li><a href="">Home</a></li>
+                <li><a href="">Student</a></li>
+                <li><a href="">Schedule</a></li>
+                <li><a href="">Room</a></li>
+                <li><a href="">Teacher</a></li>
+                <li><a href="">Subject</a></li>
+                <li><a href="">Summary</a></li>
+                
+            </ul>
+        </nav>
+        <div class="registration hideon-mobile">
+            <a href="" class="sign-up">Sign up</a>
+            <button class="btn-yellow" id="">Log In</button>
+        </div>
+        <li class="menu-button" onclick="showSidebar()">
+            <i class="ri-menu-line"></i>
+          </li>
+    </header>
+    <!--This is a  <header>
         <a href="">
             <img src="<?= Url::to("@web/images/logo.png")?>">
         </a>
@@ -130,7 +225,7 @@ AppAsset::register($this);
             <a href="" class="sign-up">Sign up</a>
             <button class="btn-yellow">Log In</button>
         </div>
-    </header>
+    </header> -->
 
     <main role="main" class="flex-shrink-0">
         <div class="container">
--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -11,15 +11,16 @@ use yii\helpers\Url;
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BHS Registry System</title>
     <link rel="stylesheet" href="<?= Yii::$app->request->baseUrl?>/css/style.css" >
-    <link rel="icon" href="/registry.system/images/logo.png">
+    <link rel="icon" href="<?= Url::to('@web/images/logo.png')?>">
     <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
+
 
 </head>
 <!-- <<<<<<< HEAD -->
 <body>
     <!-- <header>
         <a href="">
-            <img src="/registry.system/images/logo.png" alt="BHS REGISTRY SYSTEM LOGO"> 
+            <img src="/registry.system/images/logo.png')?>" alt="BHS REGISTRY SYSTEM LOGO"> 
             <img src="<?= Url::to('@web/images/images/logo.png')?>" alt="BHS REGISTRY SYSTEM LOGO">
         </a>
         <nav>
@@ -43,9 +44,10 @@ use yii\helpers\Url;
 <!-- ======= -->
 
 <body>
+   
     <!-- <header>
             <a href="">
-                <img src="/registry.system/images/logo.png" alt="BHS REGISTRY SYSTEM LOGO">
+                <img src="/registry.system/images/logo.png')?>" alt="BHS REGISTRY SYSTEM LOGO">
                 <img src="<?php // Url::to('@web/images/images/logo.png')
                             ?>" alt="BHS REGISTRY SYSTEM LOGO">
             </a>
@@ -68,14 +70,14 @@ use yii\helpers\Url;
             </div>
         </header> -->
 <!-- >>>>>>> b3bf65b (justification of index.php and removal of excess header) -->
-    <main>
+<main>
         <div id="hero">
             <div class="hero-text">
                 <div>
                     <h1>Balingasa High School</h1>
                     <h2>Registry System</h2>
                 </div>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione doloribus <br> illo, vel repellendus quas laudantium quisquam perspiciatis <br> culpa ex sed ipsum ab illum ipsam optio hic ad est iusto cum.</p>
+                <p>Online registration at the Balingasa High School. <br>Created to serve students and parents <br>as quick and responsive as possible</p>
                 <button class="btn-yellow learn" id="goAbout">Learn more</button>
             </div>
             <div class="hero-img">
@@ -87,9 +89,7 @@ use yii\helpers\Url;
                     <div class="image-wrapper-abt">
                     </div>
                     <div class="text-wrapper-abt">
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque, consequuntur natus vitae quod dolorum aliquam nisi tempore obcaecati debitis quam cum sint autem et! Cupiditate ipsa ipsam sapiente dolor voluptatibus!
-                        Tempora voluptatem maiores debitis? Natus necessitatibus omnis itaque iusto animi officiis eius, quo laboriosam temporibus architecto libero reprehenderit ab iste pariatur ex asperiores culpa quae doloremque, quaerat incidunt numquam maxime!
-                        Obcaecati, laudantium! Quasi nihil, impedit quidem ipsum, dignissimos quod minima eligendi sapiente quam, mollitia sit eveniet omnis suscipit illum amet reprehenderit velit architecto necessitatibus ab iste qui! Provident, illum consequatur.
+                        &nbsp;&nbsp;&nbsp;From Balingasa High School, currently undergoing our on-the-job training at the Bureau of Agriculture and Fisheries Engineering (BAFE), where we aim to apply our technical skills in real-world scenarios while contributing to projects, and eager to improve our knowledge of programming languages as it is an opportunity to refine our expertise, develop problem-solving capabilities, and gain hands-on experience that will prepare us for future professional challenges. Learning how to became more efficient and great programmers in the future.
                     </div>
                 </div>
         </div>
@@ -97,8 +97,7 @@ use yii\helpers\Url;
             <h2 class="people-ttl">Website Developers</h2>
             <div class="card-container">
                 <div class="card card1">
-                    <!-- <img src="/registry.system/images/cholo-card.png" alt="Van Cholo Esguerra"> -->
-                    <img src="<?= Url::to('@web/images/images/cholo-card.png') ?>" alt="Van Cholo Esguerra">
+                    <img src="<?= Url::to('@web/images/cholo-card.png')?>" alt="Van Cholo Esguerra">
                     <div class="links5">
                         <a href="https://chocoacocoa.github.io/Van-Cholo---Profile/" target="_blank"><i class="ri-pages-line"></i></a>
                         <a href="https://www.facebook.com/vancholo.esguerra.5/" target="_blank"><i class="ri-facebook-fill"></i></a>
@@ -108,7 +107,7 @@ use yii\helpers\Url;
                     </div>
                 </div>
                 <div class="card card2">
-                    <img src="<?= Url::to('@web/images/images/cudia-card.png') ?>" alt="Lucky Vincent Cudia">
+                    <img src="<?= Url::to('@web/images/cudia-card.png')?>" alt="Lucky Vincent Cudia">
                     <div class="links3">
                         <a href="https://www.facebook.com/luckyvincent.cudia?mibextid=ZbWKwL" target="_blank"><i class="ri-facebook-fill"></i></a>
                         <a href="https://github.com/Cudia0" target="_blank"><i class="ri-github-fill"></i></a>
@@ -116,13 +115,13 @@ use yii\helpers\Url;
                     </div>
                 </div>
                 <div class="card card3">
-                    <img src="<?= Url::to('@web/images/images/ken-card.png') ?>" alt="Julius Peralta">
+                    <img src="<?= Url::to('@web/images/julius-card.png')?>" alt="Julius Peralta">
                     <div class="links">
-                        <a href="#" target="_blank"><i class="ri-facebook-fill"></i></a>
+                        <a href="https://www.facebook.com/juliusper" target="_blank"><i class="ri-facebook-fill"></i></a>
                     </div>
                 </div>
                 <div class="card card4">
-                    <img src="<?= Url::to('@web/images/images/rogon-card.png') ?>" alt="Leonel Rogon">
+                    <img src="<?= Url::to('@web/images/rogon-card.png')?>" alt="Leonel Rogon">
                     <div class="links5">
                         <a href="https://rogondotdev.github.io/personal-portfolio/" target="_blank"><i class="ri-pages-line"></i></a>
                         <a href="https://www.facebook.com/rogon.dev" target="_blank"><i class="ri-facebook-fill"></i></a>
@@ -132,57 +131,56 @@ use yii\helpers\Url;
                     </div>
                 </div>
                 <div class="card card5">
-                    <img src="<?= Url::to('@web/images/images/ken-card.png') ?>" alt="Kenneth Apolong">
+                    <img src="<?= Url::to('@web/images/ken-card.png')?>" alt="Kenneth Apolong">
                     <div class="links">
-                        <a href="#" target="_blank"><i class="ri-facebook-fill"></i></a>
+                        <a href="https://www.facebook.com/ken.dqox" target="_blank"><i class="ri-facebook-fill"></i></a>
                     </div>
                 </div>
-            </div>
 
-            <div class="card-container">
-                <div class="card card1">
-                    <img src="<?= Url::to('@web/images/images/toledo-card.png') ?>" alt="Hillary Mhyles Toledo">
+                <div class="card card6">
+                    <img src="<?= Url::to('@web/images/toledo-card.png')?>" alt="Hillary Mhyles Toledo">
                     <div class="links3">
                         <a href="https://www.facebook.com/share/6s5ZNDhg7dAgBaFA/?mibextid=LQQJ4d" target="_blank"><i class="ri-facebook-fill"></i></a>
                         <a href="https://www.instagram.com/colorshin77?igsh=Y3d0dXJ3NWQ4eHdm&utm_source=qr" target="_blank"><i class="ri-instagram-line"></i></a>
                         <a href="https://l.facebook.com/l.php?u=https%3A%2F%2Fgithub.com%2FButterHoney-mhyles890%3Ffbclid%3DIwZXh0bgNhZW0CMTEAAR0LG2GU1KVdao7r_zO5QtDGX6dCIjl4H8TfhPERIsXVLY60xQByUGvHiAc_aem_5XnEy5oKPOZgzw2LvGxI6Q&h=AT13Sy0dVPHXLFDHLsSwdfaE8InLUZ52SaoK_wz-j6W0-Fd568AfLG44lgdpbA8IwMNje2k4gYakmwebUT9wkm0rp1jwQN-5qNYhbyHm4sNbXW29o5GAXzpXezFXtrGKCWrZd_R-8WNqXA0&s=1" target="_blank"><i class="ri-github-fill"></i></a>
                     </div>
                 </div>
-                <div class="card card2">
-                    <img src="<?= Url::to('@web/images/images/elaiza-card.png') ?>" alt="Elaize Rosario">
+                <div class="card card7">
+                    <img src="<?= Url::to('@web/images/elaiza-card.png')?>" alt="Elaize Rosario">
                     <div class="links2">
                         <a href="https://www.facebook.com/elaizajane.rosario" target="_blank"><i class="ri-facebook-fill"></i></a>
                         <a href="https://github.com/ElaizaJanee" target="_blank"><i class="ri-github-fill"></i></a>
                     </div>
                 </div>
-                <div class="card card3">
-                    <img src="<?= Url::to('@web/images/images/jundy-card.png') ?>" alt="Sir Jundy Dimasu-ay">
+                <div class="card card8">
+                    <img src="<?= Url::to('@web/images/jundy-card.png')?>" alt="Sir Jundy Dimasu-ay">
                     <div class="links">
-                        <a href="#" target="_blank"><i class="ri-facebook-fill"></i></a>
+                        <a href="https://www.facebook.com/dimasuayjundy" target="_blank"><i class="ri-facebook-fill"></i></a>
                     </div>
                 </div>
-                <div class="card card4">
-                    <img src="<?= Url::to('@web/images/images/eloisa-card.png') ?>" alt="Eloisa Rosario">
+                <div class="card card9">
+                    <img src="<?= Url::to('@web/images/eloisa-card.png')?>" alt="Eloisa Rosario">
                     <div class="links3">
                         <a href="https://www.facebook.com/eloisajean.rosario.54" target="_blank"><i class="ri-facebook-fill"></i></a>
                         <a href="https://github.com/LilJeanie" target="_blank"><i class="ri-github-fill"></i></a>
                         <a href="https://www.instagram.com/eloisajean.14" target="_blank"><i class="ri-instagram-line"></i></a>
                     </div>
                 </div>
-                <div class="card card5">
-                    <img src="<?= Url::to('@web/images/images/erin-card.png') ?>" alt="Erin Alonzo">
+                <div class="card card10">
+                    <img src="<?= Url::to('@web/images/erin-card.png')?>" alt="Erin Alonzo">
                     <div class="links">
                         <a href="https://www.facebook.com/share/1DXsqNLEfu/?mibextid=qi2Omg" target="_blank"><i class="ri-facebook-fill"></i></a>
                     </div>
                 </div>
             </div>
         </div>
-
+        
     </main>
     <footer>
         <div class="footer-container">
             <div class="logo-foot">
-                <img src="<?= Url::to('@web/images/images/logo.png') ?>" alt="Balingasa Higb School Registry System">
+                <img src="<?= Url::to('@web/images/logo.png')?>" alt="Balingasa Higb School Registry System">
+                <img src="<?= Url::to('@web/images/bafe-logo.png')?>" alt="Bureau of Agriculture and Fisheries Engineering">
             </div>
             <div class="links-foot">
                 <h2>WEBSITE DEVELOPERS | SPECIAL MENTIONS</h2>
@@ -231,7 +229,75 @@ use yii\helpers\Url;
                                 <h3>Mr. Jundy Dimasu-ay</h3>
                                 <p>&nbsp;&nbsp;&nbsp;IT Professional</p>
                             </div>
+                            <div class="ment-three">
+                                <h3>Gabriel Capangyarihan</h3>
+                                <p>&nbsp;&nbsp;&nbsp;Photographer</p>
+                            </div>
+                            <div class="ment-four">
+                                <h3>Ms. Ana Isabel Dupla</h3>
+                                <p>&nbsp;&nbsp;&nbsp;ICT Teacher</p>
+                            </div>
                         </div>
+                    </div>
+
+                    <div class="developers-container2">
+                        <div class="maledev">
+                            <div class="dev dev-one">
+                                <h3>Van Cholo Esguerra</h3>
+                                <p>&nbsp;&nbsp;&nbsp;Backend Web Developer</p>
+                            </div>
+                            <div class="dev dev-two">
+                                <h3>Lucky Vincent Cudia</h3>
+                                <p>&nbsp;&nbsp;&nbsp;Backend Web Developer</p>
+                            </div>
+                            <div class="dev dev-three">
+                                <h3>Kenneth Apolong</h3>
+                                <p>&nbsp;&nbsp;&nbsp;Backend Web Developer</p>
+                            </div>
+                            <div class="dev dev-four">
+                                <h3>Leonel Rogon</h3>
+                                <p>&nbsp;&nbsp;&nbsp;Frontend Web Developer</p>
+                            </div>
+                        </div>
+
+                        <div class="femaledev">
+                            <div class="dev dev-five">
+                                <h3>Hillary Mhyles Toledo</h3>
+                                <p>&nbsp;&nbsp;&nbsp;UI/UX & Graphic Designer</p>
+                            </div>
+                            <div class="dev dev-six">
+                                <h3>Elaiza Rosario</h3>
+                                <p>&nbsp;&nbsp;&nbsp;Frontend Web Developer</p>
+                            </div>
+                            <div class="dev dev-seven">
+                                <h3>Erin Alonzo</h3>
+                                <p>&nbsp;&nbsp;&nbsp;Frontend Web Developer</p>
+                            </div>
+                            <div class="dev dev-eight">
+                                <h3>Eloisa Rosario</h3>
+                                <p>&nbsp;&nbsp;&nbsp; Assistant Editor</p>
+                            </div>
+                        </div>
+
+                        <div class="mentions">
+                            <div class="ment-one">
+                                <h3>Mr. Julius Abe Peralta</h3>
+                                <p>&nbsp;&nbsp;&nbsp;IT Professional</p>
+                            </div>
+                            <div class="ment-two">
+                                <h3>Mr. Jundy Dimasu-ay</h3>
+                                <p>&nbsp;&nbsp;&nbsp;IT Professional</p>
+                            </div>
+                            <div class="ment-three">
+                                <h3>Gabriel Capangyarihan</h3>
+                                <p>&nbsp;&nbsp;&nbsp;Photographer</p>
+                            </div>
+                            <div class="ment-four">
+                                <h3>Ms. Ana Isabel Dupla</h3>
+                                <p>&nbsp;&nbsp;&nbsp;ICT Teacher</p>
+                            </div>
+                        </div>
+                        
                     </div>
                 </div>
             </div>
@@ -240,8 +306,16 @@ use yii\helpers\Url;
     </footer>
     <script>
         document.getElementById("goAbout").addEventListener("click", function() {
-            window.location.href = "#about";
-        });
+    window.location.href = "#about";
+  });
+    function showSidebar(){
+        const sidebar = document.querySelector('.sidebar')
+        sidebar.style.display = 'flex'
+    }
+    function hideSidebar(){
+        const sidebar = document.querySelector('.sidebar')
+        sidebar.style.display = 'none'
+    }
     </script>
 </body>
 
