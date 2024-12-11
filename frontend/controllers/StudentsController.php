28c22e05575a5499fe7628aa5c1a053d14f858e4
chocoacocoa <vancholo2006@gmail.com>
2024-12-11 22:22:39 +0800

    students are now fixed

--- frontend/controllers/StudentsController.php
+++ frontend/controllers/StudentsController.php
@@ -2,15 +2,14 @@
 
 namespace frontend\controllers;
 
-use frontend\models\TblOfficialFinalSchedule;
-use frontend\models\schedule\scheduleSearch;
 use frontend\models\TblOfficialStudents;
+use frontend\models\students\studentsSearch;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
 
 /**
- * StudentsController implements the CRUD actions for TblOfficialFinalSchedule model.
+ * StudentsController implements the CRUD actions for TblOfficialStudents model.
  */
 class StudentsController extends Controller
 {
@@ -33,13 +32,13 @@ class StudentsController extends Controller
     }
 
     /**
-     * Lists all TblOfficialFinalSchedule models.
+     * Lists all TblOfficialStudents models.
      *
      * @return string
      */
     public function actionIndex()
     {
-        $searchModel = new scheduleSearch();
+        $searchModel = new studentsSearch();
         $dataProvider = $searchModel->search($this->request->queryParams);
 
         return $this->render('index', [
@@ -49,30 +48,30 @@ class StudentsController extends Controller
     }
 
     /**
-     * Displays a single TblOfficialFinalSchedule model.
-     * @param int $Schedule_ID Schedule ID
+     * Displays a single TblOfficialStudents model.
+     * @param int $student_id Student ID
      * @return string
      * @throws NotFoundHttpException if the model cannot be found
      */
-    public function actionView($Schedule_ID)
+    public function actionView($student_id)
     {
         return $this->render('view', [
-            'model' => $this->findModel($Schedule_ID),
+            'model' => $this->findModel($student_id),
         ]);
     }
 
     /**
-     * Creates a new TblOfficialFinalSchedule model.
+     * Creates a new TblOfficialStudents model.
      * If creation is successful, the browser will be redirected to the 'view' page.
      * @return string|\yii\web\Response
      */
     public function actionCreate()
     {
-        $model = new TblOfficialFinalSchedule();
+        $model = new TblOfficialStudents();
 
         if ($this->request->isPost) {
             if ($model->load($this->request->post()) && $model->save()) {
-                return $this->redirect(['view', 'Schedule_ID' => $model->Schedule_ID]);
+                return $this->redirect(['view', 'student_id' => $model->student_id]);
             }
         } else {
             $model->loadDefaultValues();
@@ -83,25 +82,19 @@ class StudentsController extends Controller
         ]);
     }
 
-    public function actionGetStudents(){
-        $students = TblOfficialStudents::find()->select(['student_id', 'last_name'])->all();
-        $studentsList = \yii\helpers\ArrayHelper::map($students, 'student_id', 'last_name');
-        return $this->asJson($studentsList);
-    }
-
     /**
-     * Updates an existing TblOfficialFinalSchedule model.
+     * Updates an existing TblOfficialStudents model.
      * If update is successful, the browser will be redirected to the 'view' page.
-     * @param int $Schedule_ID Schedule ID
+     * @param int $student_id Student ID
      * @return string|\yii\web\Response
      * @throws NotFoundHttpException if the model cannot be found
      */
-    public function actionUpdate($Schedule_ID)
+    public function actionUpdate($student_id)
     {
-        $model = $this->findModel($Schedule_ID);
+        $model = $this->findModel($student_id);
 
         if ($this->request->isPost && $model->load($this->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'Schedule_ID' => $model->Schedule_ID]);
+            return $this->redirect(['view', 'student_id' => $model->student_id]);
         }
 
         return $this->render('update', [
@@ -110,29 +103,29 @@ class StudentsController extends Controller
     }
 
     /**
-     * Deletes an existing TblOfficialFinalSchedule model.
+     * Deletes an existing TblOfficialStudents model.
      * If deletion is successful, the browser will be redirected to the 'index' page.
-     * @param int $Schedule_ID Schedule ID
+     * @param int $student_id Student ID
      * @return \yii\web\Response
      * @throws NotFoundHttpException if the model cannot be found
      */
-    public function actionDelete($Schedule_ID)
+    public function actionDelete($student_id)
     {
-        $this->findModel($Schedule_ID)->delete();
+        $this->findModel($student_id)->delete();
 
         return $this->redirect(['index']);
     }
 
     /**
-     * Finds the TblOfficialFinalSchedule model based on its primary key value.
+     * Finds the TblOfficialStudents model based on its primary key value.
      * If the model is not found, a 404 HTTP exception will be thrown.
-     * @param int $Schedule_ID Schedule ID
-     * @return TblOfficialFinalSchedule the loaded model
+     * @param int $student_id Student ID
+     * @return TblOfficialStudents the loaded model
      * @throws NotFoundHttpException if the model cannot be found
      */
-    protected function findModel($Schedule_ID)
+    protected function findModel($student_id)
     {
-        if (($model = TblOfficialFinalSchedule::findOne(['Schedule_ID' => $Schedule_ID])) !== null) {
+        if (($model = TblOfficialStudents::findOne(['student_id' => $student_id])) !== null) {
             return $model;
         }
 
